126c51310f2992799045a76bd504a9ce4f096284
Patil nanda <patilnanda1914@gmail.com>
2026-04-14 16:51:39 +0530

    fixed all merge conflicts

--- create_db .py
+++ create_db .py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 
 conn = sqlite3.connect("database.db")
@@ -15,22 +14,4 @@ last_quiz_date TEXT
 """)
 
 conn.commit()
-=======
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT,
-password TEXT,
-streak INTEGER DEFAULT 0,
-last_quiz_date TEXT
-)
-""")
-
-conn.commit()
->>>>>>> 4b72cdccd7641943b955334ed39ab7a7af142874
 conn.close()
